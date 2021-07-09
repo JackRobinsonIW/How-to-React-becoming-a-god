54107f9becd832d197528dc2230eaeb678c852b4
Callum Cox <callum.cox@infinityworks.com>
2021-07-09 11:15:33 +0100

    Removed react logo, readded imports

--- introTopics/examples/whatIsAComponent/super-mega-app/src/App.tsx
+++ introTopics/examples/whatIsAComponent/super-mega-app/src/App.tsx
@@ -1,18 +1,13 @@
-import SimpleComponent from './components/SimpleComponent';
-import ComponentList from './components/ComponentList';
-import ComponentWithProps from './components/ComponentWithProps';
-import ComponentListWithProps from './components/ComponentListWithProps';
+import SimpleComponent from "./components/SimpleComponent";
+import ComponentList from "./components/ComponentList";
+import ComponentWithProps from "./components/ComponentWithProps";
+import ComponentListWithProps from "./components/ComponentListWithProps";
 
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-
       {/* <SimpleComponent /> */}
 
       {/* <ComponentList /> */}
@@ -22,6 +17,6 @@ const App = () => {
       {/* <ComponentListWithProps names={['Jack', 'Paul', 'Kate', 'Anthony']} /> */}
     </div>
   );
-}
+};
 
 export default App;
