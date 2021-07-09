86b59684e3151012a6eabaec0f79ed7c73556c02
Joseph Green <joseph.green@infinityworks.com>
2021-07-09 12:44:02 +0100

    simple use toggle hook, wip create a modal component to better display use case of the toggle hook

---
+++ advancedTopics/examples/custom-hooks/src/App.tsx
@@ -0,0 +1,19 @@
+import { useCallback, useState} from 'react';
+import './App.css';
+
+const useToggle = (initialState: boolean = false): [boolean, any] => {
+  const [state, setState] = useState<boolean>(initialState);
+
+  const toggle = useCallback((): void => setState(state => !state), []);
+  return [state, toggle]
+}
+
+function App() {
+    const [isTextChanged, setIsTextChanged] = useToggle();
+
+    return (
+        <button onClick={setIsTextChanged}>{isTextChanged ? 'Toggled' : 'Click to Toggle'}</button>
+    );
+}
+
+export default App;
