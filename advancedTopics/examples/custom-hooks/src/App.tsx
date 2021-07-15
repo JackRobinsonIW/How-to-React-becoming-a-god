77a2a1a3942361931b4a27d4396a3413eb79623d
Joe Green <joseph.green@infinityworks.com>
2021-07-15 10:09:40 +0100

    convered to modal to better display custom hook use case, styling and abstraction wip

--- advancedTopics/examples/custom-hooks/src/App.tsx
+++ advancedTopics/examples/custom-hooks/src/App.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useState} from 'react';
+import { useCallback, useState } from 'react';
 import './App.css';
 
 const useToggle = (initialState: boolean = false): [boolean, () => void] => {
@@ -8,12 +8,33 @@ const useToggle = (initialState: boolean = false): [boolean, () => void] => {
   return [state, toggle]
 }
 
-function App() {
-    const [isTextChanged, setIsTextChanged] = useToggle();
+interface ModalProps {
+  displayModal: boolean;
+  setDisplayModal: () => void
+}
 
-    return (
-        <button onClick={setIsTextChanged}>{isTextChanged ? 'Toggled' : 'Click to Toggle'}</button>
-    );
+const Modal = ({ displayModal, setDisplayModal }: ModalProps) => {
+
+  if (!displayModal) return null;
+
+  return (
+    <div style={{ position: 'absolute', alignSelf: 'center', padding: '20px', borderWidth: '1px', borderStyle: 'solid', marginTop: '500px', justifyContent: 'center', textAlign: 'center' }}>
+      <p style={{ fontSize: 20, fontWeight: 'bold' }}>Modal Window</p>
+      <p>Some text you want to show the user</p>
+      <button style={{ padding: '10px', fontSize: 20 }} onClick={setDisplayModal}>Close Modal</button>
+    </div>
+  )
+}
+
+function App() {
+  const [displayModal, setDisplayModal] = useToggle();
+
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center' }}>
+      <button style={{ marginTop: '100px', padding: '20px', fontSize: 20 }} onClick={setDisplayModal}>{displayModal ? 'Close Modal' : 'Open Modal'}</button>
+      <Modal displayModal={displayModal} setDisplayModal={setDisplayModal} />
+    </div>
+  );
 }
 
 export default App;
