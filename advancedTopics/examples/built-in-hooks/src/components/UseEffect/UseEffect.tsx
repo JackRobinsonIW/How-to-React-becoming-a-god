af61d0008432b8c6027c83cbd29cbc5e495d5757
Joe Green <joseph.green@infinityworks.com>
2021-07-16 09:52:51 +0100

    useeffect fetch data example

--- advancedTopics/examples/built-in-hooks/src/components/UseEffect/UseEffect.tsx
+++ advancedTopics/examples/built-in-hooks/src/components/UseEffect/UseEffect.tsx
@@ -1,4 +1,19 @@
+import { useEffect, useState } from "react";
 
 export const UseEffect = () => {
-    return <div>useEffect</div>
-}
+  const [error, setError] = useState<string | undefined>(undefined);
+  const [data, setData] = useState<string | undefined>(undefined);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos/1")
+      .then((response) => response.json())
+      .then((json) => setTimeout(() => setData(JSON.stringify(json)), 2000))
+      .catch((e: Error) => setError(e.message));
+  }, []);
+
+  if (error) return <div>{error}</div>;
+
+  if(data) return <div>{data}</div>
+
+  return <div>loading...</div>
+};
